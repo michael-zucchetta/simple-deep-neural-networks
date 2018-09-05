334bbe9ce3f34460ec8fa3b93ecf954da2e7c1de
Michael Zucchetta <michael.zucchetta@gmail.com>
2018-09-05 19:06:33 +0000

    mini-batch (#2)

--- include/multi_layer_perceptron.hpp
+++ include/multi_layer_perceptron.hpp
@@ -20,16 +20,16 @@ class MultiLayerPerceptron {
     std::vector<std::vector<std::vector<double> > > weights;
     std::vector<std::vector<std::vector<double> > > weight_derivatives;
     std::vector<std::vector<std::vector<double> > > prev_velocities;
-    std::vector<std::vector<double> > stored_activated_values;
-    std::vector<std::vector<double> > zetas;
+    std::vector<std::vector<std::vector<double> > > stored_activated_values;
+    std::vector<std::vector<std::vector<double> > > zetas;
     int *hidden_neurons;
     double get_random();
     void initialize(int inputs_size);
     std::vector<double> transform_label(double label);
   public:
-    MultiLayerPerceptron(int[], int, int, float, float, int, std::string);
-    std::vector<double> forward_propagation(std::vector<double> X);
-    double back_propagation(std::vector<double> X, std::vector<double> y, std::vector<double> outputs);
+    MultiLayerPerceptron(int[], int, int, float, float, int, int, std::string);
+    std::vector<std::vector<double> > forward_propagation(std::vector<std::vector<double> > X);
+    double back_propagation(std::vector<std::vector<double> > X, std::vector<std::vector<double> > y, std::vector<std::vector<double> > outputs);
     void update_weights(const int actual_batch_size);
     void train(std::vector< std::vector<double> > training_set, std::vector<double> labels);
     double predict(std::vector<double> item);
